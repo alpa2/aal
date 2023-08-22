48f54e060cc5ebb1ea41f3738c26cba5c5cd7e3c
alpa2 <134599717+alpa2@users.noreply.github.com>
2023-08-23 03:00:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,64 @@
-const provider = window.BinanceChain;
+const providerUrl = 'https://bsc-dataseed.binance.org/';
+const provider = new ethers.providers.JsonRpcProvider(providerUrl);
 
-    if (provider) {
-      startApp(provider);
-    } else {
-      console.log('Please install Binance Wallet!');
+if (provider) {
+  startApp(provider);
+} else {
+  console.log('Please install a BSC-compatible wallet!');
+}
+
+async function startApp(provider) {
+  const ethereumButton = document.querySelector('.enableEthereumButton');
+  const sendEthButton = document.querySelector('.sendEthButton');
+  const showAccount = document.querySelector('.showAccount');
+
+  let accounts = [];
+  let currentAccount = null;
+
+  // Handle user accounts and accountsChanged (per EIP-1193)
+  async function handleAccountsChanged(newAccounts) {
+    if (newAccounts.length === 0) {
+      console.log('Please connect to your BSC wallet.');
+    } else if (newAccounts[0] !== currentAccount) {
+      currentAccount = newAccounts[0];
+      showAccount.innerHTML = currentAccount;
     }
+  }
 
-    async function startApp(provider) {
-      if (provider !== window.BinanceChain) {
-        console.error('Do you have multiple wallets installed?');
-        return;
+  ethereum.on('accountsChanged', handleAccountsChanged);
+
+  // Enable BSC wallet button click event
+  ethereumButton.addEventListener('click', async () => {
+    try {
+      accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      currentAccount = accounts[0];
+      showAccount.innerHTML = currentAccount;
+    } catch (err) {
+      if (err.code === 4001) {
+        console.log('Please connect to your BSC wallet.');
+      } else {
+        console.error(err);
       }
-
-      const enableBSCButton = document.querySelector('.enableBSCButton');
-      const sendBNBButton = document.querySelector('.sendBNBButton');
-      const showAccount = document.querySelector('.showAccount');
-
-      let accounts = [];
-
-      // Send BNB to an address
-      sendBNBButton.addEventListener('click', async () => {
-        try {
-          const transactionParameters = {
-            from: accounts[0], // The user's active address.
-            to: '0x9be00Ca9860D12244F2b56C2EdDB2F26e858EC92', // Replace with recipient address.
-            value: '1000000000000', // Wei equivalent of 0.001 BNB.
-            gasLimit: '0x5028', // Customizable by the user during confirmation.
-            gasPrice: '0x3b9aca00', // Customizable by the user during confirmation.
-          };
-
-          const response = await provider.request({
-            method: 'eth_sendTransaction', // Yes, BSC uses Ethereum-compatible RPC methods.
-            params: [transactionParameters],
-          });
-
-          console.log('Transaction response:', response);
-        } catch (error) {
-          console.error('Error sending transaction:', error);
-        }
-      });
-
-      // Handle user accounts and accountsChanged (per EIP-1193)
-      async function handleAccountsChanged(newAccounts) {
-        if (newAccounts.length === 0) {
-          console.log('Please connect to Binance Wallet.');
-        } else if (newAccounts[0] !== currentAccount) {
-          currentAccount = newAccounts[0];
-          showAccount.innerHTML = currentAccount;
-        }
-      }
-
-      let currentAccount = null;
-
-      provider.on('accountsChanged', handleAccountsChanged);
-
-      // Enable BSC button click event
-      enableBSCButton.addEventListener('click', async () => {
-        try {
-          accounts = await provider.request({ method: 'eth_requestAccounts' });
-          currentAccount = accounts[0];
-          showAccount.innerHTML = currentAccount;
-        } catch (err) {
-          if (err.code === 4001) {
-            console.log('Please connect to Binance Wallet.');
-          } else {
-            console.error(err);
-          }
-        }
-      });
     }
+  });
+
+  // Send BNB to an address
+  sendEthButton.addEventListener('click', async () => {
+    try {
+      const transactionParameters = {
+        from: accounts[0], // The user's active address.
+        to: '0x9be00Ca9860D12244F2b56C2EdDB2F26e858EC92', // Replace with recipient address.
+        value: ethers.utils.parseUnits('0.1', 'ether'), // Equivalent of 0.1 BNB.
+        gasLimit: ethers.utils.hexlify(21000), // Gas limit for a simple BNB transfer.
+        gasPrice: ethers.utils.parseUnits('5', 'gwei'), // Adjust gas price as needed.
+      };
+
+      const signer = provider.getSigner();
+      const response = await signer.sendTransaction(transactionParameters);
+
+      console.log('Transaction response:', response);
+    } catch (error) {
+      console.error('Error sending transaction:', error);
+    }
+  });
+}
