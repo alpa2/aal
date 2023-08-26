02434d1a9f5159d2bdc358c9374618674ab0d710
alpa2 <134599717+alpa2@users.noreply.github.com>
2023-08-26 18:24:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function startApp(provider) {
       console.log('Please connect to MetaMask.');
     } else if (newAccounts[0] !== currentAccount) {
       currentAccount = newAccounts[0];
-      showAccount.innerHTML = currentAccount love18;
+      showAccount.innerHTML = currentAccount;
     }
   }
 
